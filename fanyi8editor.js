8946d4d9e82c4869e779da8c57121e8152544d50
noxue <yes@noxue.com>
2018-01-29 03:11:09 +0800

    完成简单的封装，实现一个页面多个编辑器，提供了公开的绑定快捷键命令函数，增加了文件上传配置（支持json和函数两种配置方式）

--- fanyi8editor.js
+++ fanyi8editor.js
@@ -1,205 +1,312 @@
-!(function (obj, $) {
+!(function ($) {
 
-	/**
-	 * 初始化操作
-	 */
-	function init($editor) {
-		$editor.setTheme('ace/theme/xcode');
-		$editor.getSession().setMode('ace/mode/markdown');
-		$editor.renderer.setShowPrintMargin(false);
-		$editor.setHighlightGutterLine(false);
-		$editor.setFontSize(16);
-		// ace.require("ace/ext/language_tools");
-		$editor.setOptions({
-			wrap: "free", //自动换行
-			// enableBasicAutocompletion: true,
-			enableSnippets: true,
-			// enableLiveAutocompletion: true
-		});
-
-		addCommands($editor);
-		$editor.setHighlightActiveLine(false);
-		$editor.focus();
-	}
+	//加载必须的js
+	loadScript();
 
 
-	//绑定快捷命令
-	function addCommands($editor) {
-		$editor.commands.addCommand({
-			name: 'parse',
-			bindKey: {
-				win: 'Ctrl-S',
-				mac: 'Command-S'
-			},
-			exec: function (editor) {
-				console.log(editor.getValue());
-			},
-			readOnly: false // 如果不需要使用只读模式，这里设置false
-		});
-	}
+	window.FanYi8Editor = function (selector, opts) {
 
 
-	function paste(editor, obj) {
+
+		//保存配置信息
+		var config = opts || {};
+
+		//ace编辑器实例
+		var editor = null;
+
+		//因为ace编辑器要求指定id来创建，考虑到同一个网页可能多个编辑器，所以就用唯一标识符来表示当前的编辑器
+		var id = "fanyi8editor-" + uuid();
+
+		//给编辑器设置唯一的id属性
+		$(selector+" .editor-content").attr("id", id);
+
+		//编辑器节点（原生js获取的dom节点）
+		var editorNode = window.document.getElementById(id);
+
+		//初始化编辑器
+		init();
 
 
-		window.document.getElementById(obj).addEventListener("paste", function (e) {
-			var cbd = e.clipboardData;
-			var ua = window.navigator.userAgent;
 
-			// 如果是 Safari 直接 return
-			if (!(e.clipboardData && e.clipboardData.items)) {
-				return;
-			}
+		//绑定拖拽上传文件相关事件
+		bindDrag();
 
-			// Mac平台下Chrome49版本以下 复制Finder中的文件的Bug Hack掉
-			if (cbd.items && cbd.items.length === 2 && cbd.items[0].kind === "string" && cbd.items[1].kind === "file" &&
-				cbd.types && cbd.types.length === 2 && cbd.types[0] === "text/plain" && cbd.types[1] === "Files" &&
-				ua.match(/Macintosh/i) && Number(ua.match(/Chrome\/(\d{2})/i)[1]) < 49) {
-				return;
-			}
+		//绑定滚动鼠标和Ctrl + - 改变文字大小
+		bindChangeFontSize();
 
-			//如果在上传图片，不处理。
-			if (uploadStatus) {
-				return;
-			}
+		//绑定粘贴上传图片事件
+		bindPaste();
 
-			for (var i = 0; i < cbd.items.length; i++) {
-				var item = cbd.items[i];
-				if (item.kind == "file") {
-					var blob = item.getAsFile();
-					if (blob.size === 0) {
-						return;
-					}
-					// blob 就是从剪切板获得的文件 可以进行上传或其他操作
-					editor.editor.setReadOnly(true);
+		var that = null;
+		/**
+		 * 初始化操作
+		 */
+		function init() {
+			//创建ace编辑器实例
+			editor = ace.edit(id);
+			editor.setTheme('ace/theme/xcode');
+			editor.getSession().setMode('ace/mode/markdown');
+			editor.renderer.setShowPrintMargin(false);
+			editor.setHighlightGutterLine(false);
+			editor.setFontSize(16);
+			editor.setOptions({
+				wrap: "free", //自动换行
+			});
 
-					uploadImg(editor, blob);
+			//阻止控制台的警告信息
+			editor.$blockScrolling = Infinity;
+
+			//让选中的行不高亮
+			editor.setHighlightActiveLine(false);
+
+			//一开始让编辑器获取焦点
+			editor.focus();
+
+
+
+			editor.on("change", function (e) {
+				$(selector+" .preview").html(marked(editor.getValue()));
+			});
+
+			that = this;
+
+			//绑定默认的命令，比如ctrl+s保存
+			bindCommands();
+		}
+
+		//绑定快捷命令
+		function addCommand(command) {
+			editor.commands.addCommand(command);
+		}
+
+		this.addCommand = addCommand;
+
+		function bindCommands(){
+			addCommand({
+				name: 'parse',
+				bindKey: {
+					win: 'Ctrl-S',
+					mac: 'Command-S'
+				},
+				exec: function (editor) {
+					console.log(editor.getValue());
+				},
+				readOnly: false // 如果不需要使用只读模式，这里设置false
+			});
+		}
+
+		/**
+		 * 绑定粘贴图片上传事件
+		 */
+		function bindPaste() {
+			window.document.getElementById(id).addEventListener("paste", function (e) {
+				var cbd = e.clipboardData;
+				var ua = window.navigator.userAgent;
+
+				// 如果是 Safari 直接 return
+				if (!(e.clipboardData && e.clipboardData.items)) {
+					return;
 				}
-			}
-		}, false);
-	}
 
-
-
-	//记录图片上传状态，限制同时只能上传一个图片
-	var uploadStatus = false;
-
-	function uploadImg(editor, file) {
-		uploadStatus = true;
-		var animateHtml = '<div class="fanyi8editor_upload_image_loader"></div>';
-		$("body").append($(animateHtml));
-
-		var formData = new FormData();
-		formData.append("file", file);
-		$.ajax({
-			url: "/upload.php",
-			type: 'POST',
-			data: formData,
-			// 告诉jQuery不要去处理发送的数据
-			processData: false,
-			// 告诉jQuery不要去设置Content-Type请求头
-			contentType: false,
-			beforeSend: function () {
-				if (window.console)
-					console.log("正在进行，请稍候");
-			},
-			success: function (res) {
-				if (res.code == 0) {
-					if(res.isImage) {
-						editor.editor.insert("![" + res.alt + "](" + res.src + ")");
-					}else{
-						editor.editor.insert("[" + res.alt + "](" + res.src + ")");
-					}
-				} else {
-					alert(res);
+				// Mac平台下Chrome49版本以下 复制Finder中的文件的Bug Hack掉
+				if (cbd.items && cbd.items.length === 2 && cbd.items[0].kind === "string" && cbd.items[1].kind === "file" &&
+					cbd.types && cbd.types.length === 2 && cbd.types[0] === "text/plain" && cbd.types[1] === "Files" &&
+					ua.match(/Macintosh/i) && Number(ua.match(/Chrome\/(\d{2})/i)[1]) < 49) {
+					return;
 				}
-				editor.editor.setReadOnly(false);
-				uploadStatus = false;
-				$(".fanyi8editor_upload_image_loader").remove();
-			},
-			error: function (responseStr) {
-				console.log("error");
-				editor.editor.setReadOnly(false);
-				uploadStatus = false;
-				$(".fanyi8editor_upload_image_loader").remove();
-			}
-		});
-	}
 
+				//如果在上传图片，不处理。
+				if (uploadStatus) {
+					return;
+				}
 
-	/**
-	 * 改变文字大小
-	 */
-	function ChangeFontSize($editor) {
-		obj.document.addEventListener('DOMContentLoaded', function (event) {
-			// chrome 浏览器直接加上下面这个样式就行了，但是ff不识别
-			obj.document.body.style.zoom = 'reset';
-			obj.document.addEventListener('keydown', function (event) {
-				if ((event.ctrlKey === true || event.metaKey === true)) {
-					var i = 0;
-					var stop = false; //是否阻止事件
-					if (event.which === 107 || event.which === 189) { //+
-						i++;
-						stop = true;
-					} else if (event.which === 109 || event.which === 187) { //-
-						i--;
-						stop = true;
+				for (var i = 0; i < cbd.items.length; i++) {
+					var item = cbd.items[i];
+					if (item.kind == "file") {
+						var blob = item.getAsFile();
+						if (blob.size === 0) {
+							return;
+						}
+						// blob 就是从剪切板获得的文件 可以进行上传或其他操作
+						editor.setReadOnly(true);
+						uploadImg(blob);
 					}
-
-					$editor.setFontSize($editor.getFontSize() + i);
-
-					//不是改变字体，就不阻止事件，否则复制粘贴之类的快捷键无法使用
-					if (stop) event.preventDefault();
 				}
 			}, false);
+		}
 
-			var EventUtil = {
-				addHandler: function (element, type, handler) {
-					if (element.addEventListener) {
-						element.addEventListener(type, handler, false);
-					} else if (element.attachEvent) {
-						element.attachEvent('on' + type, handler);
-					} else {
-						element['on' + type] = handler;
-					}
-				},
-				getEvent: function (event) {
-					return event ? event : window.event;
-				},
-				stopPropagation: function (event) {
-					event = event || window.event;
-					if (event.stopPropagation) {
-						event.stopPropagation();
-					} else {
-						event.cancelBubble = true;
+		//记录图片上传状态，限制同时只能上传一个图片
+		var uploadStatus = false;
+
+		/**
+		 * ajax方式上传图片
+		 * @param file 文件的内容
+		 */
+		function uploadImg(file) {
+			uploadStatus = true;
+			var animateHtml = '<div class="fanyi8editor_upload_image_loader"></div>';
+			$("body").append($(animateHtml));
+
+			var formData = new FormData();
+			formData.append(config.upload.name, file);
+
+			if (config.upload.kvs) {
+
+				var kvs = config.upload.kvs;
+
+				//如果是一个函数，就调用并获取返回值，配置一个函数的区别是，每次都会调用
+				if (typeof kvs === "function") {
+					kvs = kvs();
+				}
+
+				if (isJSON(kvs)) {
+					for (var k in kvs) {
+						formData.append(k, kvs[k]);
 					}
 				}
-			};
 
-			function handleMouseWheel(event) {
-				if (!(event.ctrlKey === true || event.metaKey === true)) return;
-
-				EventUtil.stopPropagation(event);
-				event = EventUtil.getEvent(event);
-				var value = event.wheelDelta || -event.detail;
-				var delta = Math.max(-1, Math.min(1, value));
-				var i = 0;
-
-				if (delta > 0) { //+
-					i++;
-				} else { //-
-					i--;
-				}
-				$editor.setFontSize($editor.getFontSize() + i);
-
-				event.preventDefault();
 			}
 
-			EventUtil.addHandler(obj.document, 'mousewheel', handleMouseWheel);
-			EventUtil.addHandler(obj.document, 'DOMMouseScroll', handleMouseWheel);
+			//上传文件
+			$.ajax({
+				url: config.upload.url,
+				type: 'POST',
+				data: formData,
+				// 告诉jQuery不要去处理发送的数据
+				processData: false,
+				// 告诉jQuery不要去设置Content-Type请求头
+				contentType: false,
+				beforeSend: function () {
+					if (window.console)
+						console.log("正在进行，请稍候");
+				},
+				success: function (res) {
+					if (res.code == 0) {
+						if (res.isImage) {
+							editor.insert("![" + res.alt + "](" + res.src + ")");
+						} else {
+							editor.insert("[" + res.alt + "](" + res.src + ")");
+						}
+					} else {
+						alert(res);
+					}
+					editor.setReadOnly(false);
+					uploadStatus = false;
+					$(".fanyi8editor_upload_image_loader").remove();
+				},
+				error: function (responseStr) {
+					console.log("error");
+					editor.setReadOnly(false);
+					uploadStatus = false;
+					$(".fanyi8editor_upload_image_loader").remove();
+				}
+			});
+		}
 
-		}, false);
-	}
+
+		/**
+		 * 改变文字大小
+		 */
+		function bindChangeFontSize() {
+			window.document.addEventListener('DOMContentLoaded', function (event) {
+				// chrome 浏览器直接加上下面这个样式就行了，但是ff不识别
+				window.document.body.style.zoom = 'reset';
+				window.document.addEventListener('keydown', function (event) {
+					if ((event.ctrlKey === true || event.metaKey === true)) {
+						var i = 0;
+						var stop = false; //是否阻止事件
+						if (event.which === 107 || event.which === 189) { //+
+							i++;
+							stop = true;
+						} else if (event.which === 109 || event.which === 187) { //-
+							i--;
+							stop = true;
+						}
+
+						editor.setFontSize(editor.getFontSize() + i);
+
+						//不是改变字体，就不阻止事件，否则复制粘贴之类的快捷键无法使用
+						if (stop) event.preventDefault();
+					}
+				}, false);
+
+				var EventUtil = {
+					addHandler: function (element, type, handler) {
+						if (element.addEventListener) {
+							element.addEventListener(type, handler, false);
+						} else if (element.attachEvent) {
+							element.attachEvent('on' + type, handler);
+						} else {
+							element['on' + type] = handler;
+						}
+					},
+					getEvent: function (event) {
+						return event ? event : window.event;
+					},
+					stopPropagation: function (event) {
+						event = event || window.event;
+						if (event.stopPropagation) {
+							event.stopPropagation();
+						} else {
+							event.cancelBubble = true;
+						}
+					}
+				};
+
+				function handleMouseWheel(event) {
+					if (!(event.ctrlKey === true || event.metaKey === true)) return;
+
+					EventUtil.stopPropagation(event);
+					event = EventUtil.getEvent(event);
+					var value = event.wheelDelta || -event.detail;
+					var delta = Math.max(-1, Math.min(1, value));
+					var i = 0;
+
+					if (delta > 0) { //+
+						i++;
+					} else { //-
+						i--;
+					}
+					editor.setFontSize(editor.getFontSize() + i);
+
+					event.preventDefault();
+				}
+
+				EventUtil.addHandler(window.document, 'mousewheel', handleMouseWheel);
+				EventUtil.addHandler(window.document, 'DOMMouseScroll', handleMouseWheel);
+
+			}, false);
+		}
+
+
+		/**
+		 * 拖拽上传
+		 */
+		function bindDrag() {
+			var obj = document.getElementById(id);
+			obj.addEventListener("dragenter", handler, false);
+			obj.addEventListener("dragover", handler, false);
+			obj.addEventListener("drop", upload, false);
+			function upload(e) {
+				var e = e || window.event;
+				handler(e);
+				var files = e.dataTransfer.files;
+
+				for (var i = 0, il = files.length; i < il; i++) {
+					//如果是文件夹，就不处理
+					if (files[i].size == 0) continue;
+					uploadImg(files[i]);
+				}
+			}
+
+			function handler(e) {
+				var e = e || window.event;
+				e.preventDefault ? e.preventDefault() : e.returnValue = false;
+				e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
+			}
+		}
+
+	};
 
 
 	/**
@@ -207,7 +314,7 @@
 	 */
 	function loadScript() {
 		var tmp_node = 'script_fanyi8_' + uuid();
-		obj.document.write('<script id="' + tmp_node + '"></script>')
+		window.document.write('<script id="' + tmp_node + '"></script>')
 		var thisNode = $("#" + tmp_node).prev("script");
 		$('#' + tmp_node).remove();
 
@@ -230,63 +337,10 @@
 		return uuid;
 	}
 
-
-	obj.FanYi8Editor = {
-
-		opts: {},
-		editor: null,
-		editorNode:null,
-		Create: function (selector, opts) {
-
-			//加载必须的js
-			loadScript();
-			var that = this;
-			this.opts = opts || {};
-			var id = "fanyi8editor-" + uuid();
-			this.editorNode = window.document.getElementById(id);
-			$(selector).attr("id", id);
-			this.editor = ace.edit(id);
-			init(that.editor);
-			initDragToUpload(that);
-			ChangeFontSize(that.editor);
-			paste(that, id);
-
-			that.editor.on("change", function (e) {
-				$("#preview").html(marked(that.editor.getValue()));
-			});
-
-		}
-	};
-
-
-
-	/**
-	 * 拖拽上传
-	 */
-	function initDragToUpload(editor) {
-		var obj = document.getElementById("editor");
-		obj.addEventListener("dragenter", handler, false);
-		obj.addEventListener("dragover", handler, false);
-		obj.addEventListener("drop", upload, false);
-		function upload(e) {
-			var e = e || window.event;
-			handler(e);
-			var files = e.dataTransfer.files;
-
-			for (var i = 0, il = files.length; i < il; i++) {
-				//如果是文件夹，就不处理
-				if(files[i].size == 0) continue;
-				uploadImg(editor, files[i]);
-			}
-		}
-
-		function handler(e) {
-			var e = e || window.event;
-			e.preventDefault ? e.preventDefault() : e.returnValue = false;
-			e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
-
-
-		}
+	//判断是否是json对象
+	function isJSON(obj) {
+		var isjson = typeof(obj) == "object" && Object.prototype.toString.call(obj).toLowerCase() == "[object object]" && !obj.length;
+		return isjson;
 	}
 
-})(window, $);
+})($);
--- upload.php
+++ upload.php
@@ -1,27 +1,24 @@
 <?php
 
-if (($_FILES["file"]["type"] == "image/png") || true)
+$name = "image";
+
+if (($_FILES[$name]["type"] == "image/png") || true)
 {
-    if ($_FILES["file"]["error"] > 0)
+    if ($_FILES[$name]["error"] > 0)
     {
         echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
     }
     else
     {
-//        echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-//        echo "Type: " . $_FILES["file"]["type"] . "<br />";
-//        echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-//        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-        $ext = substr($_FILES["file"]["name"], strrpos($_FILES["file"]["name"], '.') + 1);
+        $ext = substr($_FILES[$name]["name"], strrpos($_FILES[$name]["name"], '.') + 1);
         $fileName = "upload/" . rand(1000,1000000).".".$ext;
         if (file_exists($fileName))
         {
-            echo $_FILES["file"]["name"] . " already exists. ";
+            echo $_FILES[$name]["name"] . " already exists. ";
         }
         else
         {
-            move_uploaded_file($_FILES["file"]["tmp_name"],
+            move_uploaded_file($_FILES[$name]["tmp_name"],
                 $fileName);
 
             header('Content-Type: application/json');
@@ -29,7 +26,7 @@ if (($_FILES["file"]["type"] == "image/png") || true)
             echo  json_encode([
                 "code"=>0,
                 "src"=>$fileName,
-                "alt"=>$_FILES["file"]["name"],
+                "alt"=>$_FILES[$name]["name"],
                 "isImage"=>in_array(strtolower($ext),["jpg","gif","jpeg","bmp","png"]),
                 "errMsg"=>""
             ]);
